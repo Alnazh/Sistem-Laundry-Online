47a6b9339f0dbcbfe26e1b3cbbb064311e66943d
Alnazh <alnzh08@gmail.com>
2025-07-14 16:41:32 +0700

    Fix deployment: update app.py

--- app.py
+++ app.py
@@ -346,4 +346,8 @@ def submit_order():
         }), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+else:
+    # Untuk eksekusi dengan gunicorn
+    gunicorn_app = app
